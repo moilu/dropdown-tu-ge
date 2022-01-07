9b6af7f305eb4ed1319f7a06df79d1a269841f5b
moilu <moisesluna0390@gmail.com>
2022-01-06 22:46:48 -0800

    progress on dropdown challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import axios from 'axios';
 
-function App() {
+const api = axios.create({
+  baseURL: `https://back.implementaconbubo.com/v1/sales/customer/?simple=true`
+});
+
+const App = () => {
+
+const [myOptions, setMyOptions] = useState([])
+
+async function getData() {
+  // fetch('https://back.implementaconbubo.com/v1/sales/customer/?simple=true')
+  //   .then((response => {
+  //     return response.json();
+  //   })).then((res) => {
+  //     console.log(res.data);
+  //     for (var i = 0; i < res.data.lenght; i++) {
+  //       myOptions.push(res.data[i])
+  //     }
+  //     console.log(myOptions)
+  //   })
+  fetch('https://back.implementaconbubo.com/v1/sales/customer/?simple=true', {
+    mode: 'no-cors',
+    headers: {
+      // 'Allow': 'APIKEY',
+    }
+  }).then((response) => {
+    return response.json
+  }).then((response) => {
+    console.log(response)
+  })
+}
+
+const getDataFromAPI = () => {
+  console.log("Options Fetched from API")
+
+  fetch('http://dummy.restapiexample.com/api/v1/employees').then((response) => {
+    return response.json()
+  }).then((res) => {
+    for (var i = 0; i < res.data.length; i++) {
+      myOptions.push(res.data[i].employee_name)
+    }
+    setMyOptions(myOptions)
+  })
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' >
+      <h3>Ingresa tu búsqueda</h3>
+      <Autocomplete
+        className="autocomplete"
+        freeSolo
+        autoComplete
+        autoHighlight
+        options={myOptions}
+        renderInput={(params) => (
+          <TextField {...params}
+            onChange={getDataFromAPI, getData}
+            variant="outlined"
+            label="Costumers"
+          />
+        )}
+      />
     </div>
   );
 }
